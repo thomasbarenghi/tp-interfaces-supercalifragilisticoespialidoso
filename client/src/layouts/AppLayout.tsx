9b82e0c55d2036371935e155a7231b2b40ebf061
Mateo Agustín Sanchez Sa <109445757+Mateo1223@users.noreply.github.com>
2026-06-10 18:37:32 -0300

    Refactor `AppLayout` to use fragments and apply full-height flex column layout to `#root` in CSS (#26)
    Co-authored-by: thomasbarenghi <thomasbarenghi@gmail.com>

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -3,11 +3,11 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const AppLayout = () => (
-  <div>
+  <>
     <Header />
     <Outlet />
     <Footer />
-  </div>
+  </>
 )
 
 export default AppLayout
